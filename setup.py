4748edcdbdf6963da60213f050d1b12306609757
Philip Peterson <philip.c.peterson@gmail.com>
2013-05-13 21:10:03 -0400

    Add Andrew's starryexpanse email addy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='ReferenceFrame',
       version='1.0',
       description='Starry Expanse reference image browser',
       author='Andrew Kennedy, Philip Peterson',
-      author_email='andrew@akenn.org, philip@starryexpanse.com',
+      author_email='andrew@starryexpanse.com, philip@starryexpanse.com',
       url='http://github.com/starryexpanse/Reference-Browser',
       packages=['referenceframe'],
      )
